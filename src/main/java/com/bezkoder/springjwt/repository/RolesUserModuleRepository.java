d69be206cebae0bd2cfca023e60ec5cad0556789
DNH <duongnh.nava@gmail.com>
2023-02-10 09:39:42 +0700

    update

--- src/main/java/com/bezkoder/springjwt/repository/RolesUserModuleRepository.java
+++ src/main/java/com/bezkoder/springjwt/repository/RolesUserModuleRepository.java
@@ -4,11 +4,6 @@ import com.bezkoder.springjwt.models.RolesUserModule;
 import com.bezkoder.springjwt.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.TypedQuery;
 import java.util.List;
 
 
